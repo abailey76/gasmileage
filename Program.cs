d56ca9f5770cc91d3e9a10b537715109971eb0b7
Adam Bailey <abailey76@duck.com>
2021-11-17 12:46:39 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace gasmileage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int milesDriven, gasConsumed, gasMileage;
+
+            Console.WriteLine("How many miles did you drive?");
+
+            milesDriven = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many gallons of gas did you burn?");
+
+            gasConsumed = Convert.ToInt32(Console.ReadLine());
+
+            gasMileage = milesDriven / gasConsumed;
+
+            Console.WriteLine("Your gas mileage is: {0}", gasMileage);
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
